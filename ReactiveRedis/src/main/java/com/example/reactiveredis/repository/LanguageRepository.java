3233e8ec5182e940157277811704394af9dae929
AnatolSalau <salovanatoly5@mail.ru>
2024-01-19 20:50:06 +0300

    ReactiveRedis

--- ReactiveRedis/src/main/java/com/example/reactiveredis/repository/LanguageRepository.java
+++ ReactiveRedis/src/main/java/com/example/reactiveredis/repository/LanguageRepository.java
@@ -1,4 +1,23 @@
 package com.example.reactiveredis.repository;
 
+import com.example.reactiveredis.model.Language;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+@Component
 public class LanguageRepository {
+      private ReactiveRedisOperations<Long, Language> template;
+
+      @Autowired
+      public LanguageRepository(ReactiveRedisOperations<Long, Language> template) {
+            this.template = template;
+      }
+
+      public Flux<Language> findAll()  {
+            return template
+                  .<Long, Language> opsForHash()
+                  .values()
+      }
 }
